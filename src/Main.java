48502281d320b13b112d3af07aefa4e08f44aa37
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2021-01-27 01:48:24 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
+import javax.swing.UIManager;
+
 public class Main{
 	public static void main(String args[]) {
 		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
 			new TextEditor();
 		}
 		catch(Exception e) {
 			System.out.println(e);
 		}
-		
+
 	}
 }
