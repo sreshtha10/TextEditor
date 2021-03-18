7c942f3388e090059c982f956fbf3a99f44feef3
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2021-03-18 15:20:27 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-import javax.swing.UIManager;
+/*Concepts implemented:
+ * 1. swing and awt for GUI.
+ * 2. Classes and objects 
+ * 3. Constructor, Anonymous class
+ * 4. static, final and access modifiers. 
+ * 5. File Handling 
+ * 6. Exception Handling
+ * 7. Interfaces, method overriding (Runtime Polymorphism).
+ * 8. Encapsulation,Multithreading (basic)
+ * 9. Important keywords like this,final etc.
+ * 10.Garbage collection and finalize.
+ */
 
+
+
+import javax.swing.UIManager;
 public class Main{
 	public static void main(String args[]) {
 		try {
